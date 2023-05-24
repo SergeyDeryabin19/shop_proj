1396506c29b656ef8f29f5f7131becfc1d3ee949
SergeyDeryabin19 <deryabinsergey1994@gmail.com>
2023-05-24 23:37:59 +0300

    new page genres

--- src/directories/views.py
+++ src/directories/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+
+def genres_vew(request):
+    genres = models.Genres.objects.all()  
+    return render(
+        request, 
+        template_name = "book_parametrs/genres.html", 
+        context={'objects': genres}
+        )
--- src/proj/urls.py
+++ src/proj/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from directories import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('genres.html', views.genres_vew),
 ]
