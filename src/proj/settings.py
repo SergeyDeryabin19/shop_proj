fc7ab93d3d2cea59b7d2e3fee0ea1f0249b8fadf
SergeyDeryabin19 <deryabinsergey1994@gmail.com>
2023-05-25 00:31:33 +0300

    all four pages v2

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from . import local_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!yuz!muxuzof%p4sg0q7af*u1u1m^2(8ifrfg)#a!71aikpnyt'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = local_settings.DEBUG
 
 ALLOWED_HOSTS = ['SergeyDeryabin.pythonanywhere.com' , '127.0.0.1']
 
